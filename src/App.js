bc11f83c009f6cffb5f06b48aa932a4fbdd96d82
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2023-01-14 01:37:35 +0600

    React Sticky Nav

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import CogIcon from '@rsuite/icons/legacy/Cog';
+import HomeIcon from '@rsuite/icons/legacy/Home';
+import { Affix, Nav, Navbar } from 'rsuite';
+import 'rsuite/dist/rsuite.min.css';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<Affix>
+		<Navbar>
+    <Navbar.Brand href="#">RSUITE</Navbar.Brand>
+    <Nav>
+      <Nav.Item icon={<HomeIcon />}>Home</Nav.Item>
+      <Nav.Item>News</Nav.Item>
+      <Nav.Item>Products</Nav.Item>
+      <Nav.Menu title="About">
+        <Nav.Item>Company</Nav.Item>
+        <Nav.Item>Team</Nav.Item>
+        <Nav.Menu title="Contact">
+          <Nav.Item>Via email</Nav.Item>
+          <Nav.Item>Via telephone</Nav.Item>
+        </Nav.Menu>
+      </Nav.Menu>
+    </Nav>
+    <Nav pullRight>
+      <Nav.Item icon={<CogIcon />}>Settings</Nav.Item>
+    </Nav>
+  </Navbar>
+		</Affix>
+	
+		
+		<div class="container">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos, perferendis optio voluptatum nemo repudiandae minus corporis modi quos voluptates vel nulla quas quisquam provident delectus nihil perspiciatis facilis et!</p>
+
+		</div>
     </div>
   );
 }
